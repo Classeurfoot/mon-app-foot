3b9b6d9447003996a61314e545159f1954c2f139
Classeurfoot <benjamin_lafleur@hotmail.com>
2026-03-03 09:09:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -195,6 +195,10 @@ MENU_ARBO = {
 def load_data():
     try:
         df = pd.read_csv("matchs.csv", sep=None, engine="python", on_bad_lines='skip')
+        df = pd.read_csv("matchs.csv", sep=",", encoding="utf-8-sig")
+
+# --- LIGNE DE DÉBOGAGE À AJOUTER ---
+st.error(f"🔍 DÉBOGAGE : L'application arrive à lire {len(df)} lignes dans le fichier brut.")
         df = df.dropna(subset=['Domicile', 'Extérieur'])
         df.columns = df.columns.str.strip()
         
@@ -1128,6 +1132,7 @@ elif st.session_state.page == 'arborescence':
             mask = df['Compétition'].str.contains(noeud_actuel, na=False, case=False)
             df_final = df[mask]
             afficher_resultats(df_final)
+
 
 
 
